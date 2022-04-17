165365b770f9db0fe6a344357cd03c1f97709955
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-04-17 15:00:57 +0530

    Middleware RequestValidator Category and Product

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,12 +1,12 @@
 const productController = require('../controllers/products.controller');
-
+const {requestValidator} = require('../middleware');
 module.exports = function(app){
 
     //Route for POST request to create the product
-    app.post('/ecomm/api/v1/products', productController.create);
+    app.post('/ecomm/api/v1/products',[requestValidator.validateProductRequest], productController.create);
 
     //Route for PUT request to update the product
-    app.put('/ecomm/api/v1/products/:id', productController.update);
+    app.put('/ecomm/api/v1/products/:id',[requestValidator.validateProductRequest] ,productController.update);
 
     //Route for DELETE request to delete the product
     app.delete('/ecomm/api/v1/products/:id', productController.delete);
@@ -18,6 +18,6 @@ module.exports = function(app){
     app.get('/ecomm/api/v1/products', productController.findAll);
 
     //Route for GET request to get the all the products of the given category id
-    app.get('/ecomm/api/v1/categories/:categoryId/products', productController.getProductsUnderCategory);
+    app.get('/ecomm/api/v1/categories/:categoryId/products',[requestValidator.validateCategoryInRequestParams], productController.getProductsUnderCategory);
     
 }
