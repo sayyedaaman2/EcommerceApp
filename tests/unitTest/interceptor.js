37159a6f7fa182542ad64fd02be46347c9f65dd9
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-04-21 03:00:45 +0530

    Adding some tests

---
+++ tests/unitTest/interceptor.js
@@ -0,0 +1,25 @@
+module.exports = {
+    mockRequest : ()=>{
+        const req = {};
+        /**
+         * body
+         * params
+         * query
+         */
+        req.body = jest.fn().mockReturnValue(req);
+        req.params = jest.fn().mockReturnValue(req);
+        req.query = jest.fn().mockReturnValue(req);
+        return req;
+    },
+
+    mockResponse : ()=>{
+        const res = {};
+        /**
+         * status
+         * send
+         */
+        res.status = jest.fn().mockReturnValue(res);
+        res.send = jest.fn().mockReturnValue(res);
+        return res;
+    }
+}
