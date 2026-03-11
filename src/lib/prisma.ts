8e45656c0b820281be5ba2f7b1e257b6a3896461
sayyedaaman2 <sayyedaaman9@gmail.com>
2026-03-11 16:35:11 +0530

    config: server and database

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,6 +6,8 @@ const adapter = new PrismaMariaDb({
   user: process.env.DATABASE_USER!,
   password: process.env.DATABASE_PASSWORD!,
   database: process.env.DATABASE_NAME!,
+  allowPublicKeyRetrieval: true,
+  ssl: false,
 });
 
 const prisma = new PrismaClient({ adapter });
